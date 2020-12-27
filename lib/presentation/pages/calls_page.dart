22e960aac2802f7e17faab239a54d9d06917c6ab
ardiprima_ <ardiprima27@gmail.com>
2020-12-27 19:08:34 +0700

    done call page

--- lib/presentation/pages/calls_page.dart
+++ lib/presentation/pages/calls_page.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:whatsme/presentation/pages/sub_pages/single_item_call_page.dart';
+import 'package:whatsme/presentation/widgets/theme/style.dart';
 
 class CallsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Calls Page"),),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context,index)=>SingleItemCallPage(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        backgroundColor: primaryColor,
+        child: Icon(Icons.add_call,color: Colors.white,),
+      ),
     );
   }
 }
